4177c033fe6902cec300c58494f16242e6f05bde
smmakowski <makowski.sm@gmail.com>
2017-10-23 15:17:34 -0700

    Complete Hello World

---
+++ HelloUMUC.java
@@ -0,0 +1,12 @@
+/*
+  Print "Hello UMUC!" to standard outputs
+*/
+
+public class HelloUMUC {
+  public static void main(String[] args) {
+    // Display the string
+    String name = "Stephen Makowski";
+    System.out.println("Hello UMUC! My Name is " + name + '.');
+    System.out.println("I look forward to learning more about Java!");
+  }
+}
