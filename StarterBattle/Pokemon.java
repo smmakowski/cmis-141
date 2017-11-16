02137c7580b0ca588881d46a8e1c0bb335f93425
smmakowski <makowski.sm@gmail.com>
2017-11-16 14:29:23 -0800

    complete draft opening and name/rival/pokemonchoice prompts

--- StarterBattle/Pokemon.java
+++ StarterBattle/Pokemon.java
@@ -24,25 +24,48 @@ public class Pokemon {
     sDefense = sD;
     speed = s;
   }
-  // public void setNickname(String name) {
-  //   Scanner scan = new Scanner(System.in);
-  //   String resp;
-  //   String nickname = name;
-  //   boolean pickingName = true;
-  //   while (pickingName) {
-  //     while ()
-  //     System.out.println("Would you like to give a nickname to " + name + "?");
-  //     nickname = scan.nextLine();
-  //
-  //     System.out.println("You have chosen the nickname \"" + nickname + "")
-  //
-  //   }
-  //   System.out.println("Would you like to give a nickname to " + name + "?");
-  //   nickname = scan.nextLine();
-  //
-  //   System.out.println("You have chosen the nickname \"" + nickname + "")
-  //
-  // }
+  public void setNickname() {
+    Scanner scan = new Scanner(System.in);
+    String nickname = name;
+    String choice = "";
+    boolean pickingName = true;
+    while (true) {
+      System.out.println("Would you like to give " + this.name + " a nickname ('y' for yes 'n' for no, non case-sensitive)?");
+      choice = scan.next().toLowerCase();
+
+      if (choice.equals("y")) {
+        break;
+      } else if (choice.equals("n")) {
+        System.out.println("Okay then! The default name is cool too...");
+        return;
+      } else {
+        System.out.println("Invalid input! Please try again!");
+      }
+    }  
+    
+
+    while (pickingName) {
+      System.out.println("What nickname would you like to give a nickname to " + name + "?");
+      nickname = scan.next();
+      System.out.println("You have chosen the nickname \"" + nickname + ".");
+
+      while (true) {
+        System.out.println("Are you sure? This is literally the only part of the game where we'll try to confirm your choice!");
+        System.out.println("Enter 'Yes' to confirm or 'No' to change name (Answer is not case-sensitive):");
+        choice = scan.next().toLowerCase();
+        if (choice.equals("yes")) {
+          this.name = nickname;
+          pickingName = false;
+          break;
+        } else if (choice.equals("no")) {
+          break;
+        } else {
+          System.out.println("That's not a valid answer!");
+        }
+      }
+    }
+  }
+
   public void call() {
     System.out.println(this.name + "-" + name + "!!!");
   }
--- StarterBattle/PokemonBattle.java
+++ StarterBattle/PokemonBattle.java
@@ -1,8 +1,70 @@
+import java.util.Scanner;
+
 public class PokemonBattle {
   public static void main(String[] args) {
-    Pokemon charmander = new Pokemon("Bulbasaur", "Grass", 5, 45, 49, 49, 65, 65, 45);
-    Pokemon bulbasaur = new Pokemon("Charmander", "Fire", 5, 39, 52, 43, 60, 50, 65);
+    Scanner scan = new Scanner(System.in);
+    boolean choosingPokemon = true;
+    String pick = "";
+    String conf = "";
+    String yourName = "";
+    Pokemon yourPokemon = null;
+    String rivalName = "";
+    Pokemon rivalPokemon= null;
+    Pokemon bulbasaur = new Pokemon("Bulbasaur", "Grass", 5, 45, 49, 49, 65, 65, 45);
+    Pokemon charmander = new Pokemon("Charmander", "Fire", 5, 39, 52, 43, 60, 50, 65);
     Pokemon squirtle = new Pokemon("Squirtle", "Water", 5, 44, 48, 65, 50, 64, 43);
-    charmander.call();
+
+    System.out.println("Professor: Welcome to my lab! I see you're here about the ad I put up for human test subjects!?");
+    System.out.println("Professor: No? oh you must be here to pick your Pokemon, then! Why didn't you day so?");
+    System.out.println("Professor: Oops, I didn't catch your name! What was it again (Please enter name)?");
+    yourName = scan.next();
+    System.out.println("Professor: " + yourName + " is it? Well nice to meet you!");
+
+    System.out.println("Oh, and this is my ___. He's looking to start his adventure too. I think his name is... (Please enter name):");
+    rivalName = scan.next();
+    System.out.println(rivalName + ": That's right! my name is " + rivalName + ", and don't you forget it!");
+
+    System.out.println("Professor: Well.. let's get to it! I have three Pokemon on the table here. Sorry.. but all the others were taken!");
+    System.out.println("Professor: We have... "); // insert names and bsaci type
+    System.out.println("Professor: Think hard! because once you call dibs, you cannot undo it!");
+
+    while (choosingPokemon) {
+        pick = scan.next();
+        switch (pick) {
+          case "bulbasaur" :
+            yourPokemon = bulbasaur;
+            rivalPokemon = charmander;
+            choosingPokemon = false;
+            break;
+          case "charmander" :
+          yourPokemon = charmander;
+            rivalPokemon = squirtle;
+            choosingPokemon = false;
+            break;
+          case "squirtle" :
+            yourPokemon = squirtle;
+            rivalPokemon = bulbasaur;
+            choosingPokemon = false;
+            break;
+          default:
+            System.out.println("I don't have that one! Please try picking again");
+        }
+      }
+
+
+    System.out.println("You have chosen " + yourPokemon.name + ", the " + yourPokemon.type + " Pokemon.");
+    yourPokemon.setNickname();
+
+    System.out.println(rivalName + ": Fine Noob, I'll take " + rivalPokemon.name + "! It's a " + rivalPokemon.type +
+    " , so it's better anyway!");
+
+    System.out.println(rivalName + ": Hey I have an idea! Let's battle!");
+
+
+    // // begin battle
+    // while (inBattle) {
+    //
+    // }
+
   }
 }
