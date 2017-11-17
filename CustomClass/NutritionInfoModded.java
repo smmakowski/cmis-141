0d5af69d4638dfff10229461e06c9e82a8b55a59
smmakowski <makowski.sm@gmail.com>
2017-11-17 15:54:27 -0800

    Remove uneccesary direcotry; add files for discussion response

---
+++ CustomClass/NutritionInfoModded.java
@@ -0,0 +1,35 @@
+/* File: NutritionInfo.java
+ * Author: Vanessa Abrahams
+ * Date: November 16, 2017
+ * Purpose: Provide Java code for a simple class of your choice. Be sure to include at least one constructor, two methods and two fields. The fields should be private.
+Create a test class to construct and call the methods of your class.
+ */
+
+public class NutritionInfo {
+
+    // initialize Nutrition fields
+    private String name;
+    private int age;
+    private short bmi;
+    private int calories;
+
+    // getters to access field values
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public short getBmi() {
+        return bmi;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+   
+    
+
+}
---
+++ CustomClass/PeopleInfoModded.java
@@ -0,0 +1,33 @@
+/* File: NutritionInfo.java
+ * Author: Vanessa Abrahams
+ * Date: November 16, 2017
+ * Purpose: Provide Java code for a simple class of your choice. Be sure to include at least one constructor, two methods and two fields. The fields should be private.
+Create a test class to construct and call the methods of your class.
+ */
+
+public class PeopleInfo {
+    public static void main(String[] args) {
+        //
+        NutritionInfo george = new NutritionInfo("George");
+	george.setName("George");
+	george.setAge(40);
+	george.setBmi(20);
+	george.setCalories(2400);
+
+	NutritionInfo carrie = new NutritionInfo("Carrie");
+	carrie.setName("Carrie");
+	carrie.setAge(23);
+	carrie.setBmi(22);
+	carrie.setCalories(2000);
+
+	 // Print results
+   	 System.out.println(george.getName() + " is " + george.age + " years old ");
+	 System.out.println(george.getName() + " has a BMI of " + george.bmi);
+    	 System.out.println(george.getName() + " should consume 2400 calories a day."); 
+
+	 System.out.println(carrie.getName() + " is " + carrie.age + " years old ");
+	 System.out.println(carrie.getName() + " has a BMI of " + carrie.bmi);
+    	 System.out.println(carrie.getName() + " should consume 2000 calories a day."); 
+
+    }
+}
