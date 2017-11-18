828930cf17dd4239f54842a03aeffd9249903739
smmakowski <makowski.sm@gmail.com>
2017-11-17 16:40:41 -0800

    debug files for repsonse

--- CustomClass/NutritionInfoModded.java
+++ CustomClass/NutritionInfoModded.java
@@ -5,13 +5,38 @@
 Create a test class to construct and call the methods of your class.
  */
 
-public class NutritionInfo {
-
+public class NutritionInfoModded {
     // initialize Nutrition fields
     private String name;
     private int age;
     private short bmi;
     private int calories;
+    /* If you want to instantiate classes you need to create the constructor method so
+    you'll have a method to run when you use the 'new' keyword */
+    public NutritionInfoModded(String n) {
+      name = n; /* set name based on parameters; Note: Make sure the paramters are named similar, but different to fields to avoid variable collision */
+      age = 0; /* default values here and below */
+      bmi = 0;
+      calories = 0;
+    }
+
+    /* You use 'set methods in PeopleInfo.java that do not exist
+    make sure to include them in your class. The 'symbol not found' error that you get is
+    the result of the PeopleInfo.java looking for the method in the NutritionInfo class and
+    not finding it */
+
+    /* Alternatively you can also set these fields by adding them as parameters in the
+    constructor function like adding a 'name' in the above constructor */
+
+    public void setAge(int age) {
+      this.age = age;
+    }
+    public void setBmi(short bmi) {
+      this.bmi = bmi;
+    }
+    public void setCalories(int calories) {
+      this.calories = calories;
+    }
 
     // getters to access field values
     public String getName() {
@@ -19,17 +44,15 @@ public class NutritionInfo {
     }
 
     public int getAge() {
-        return age;
+        return this.age;
     }
 
     public short getBmi() {
-        return bmi;
+        return this.bmi;
     }
 
     public int getCalories() {
-        return calories;
+        return this.calories;
     }
-   
-    
 
 }
--- CustomClass/PeopleInfoModded.java
+++ CustomClass/PeopleInfoModded.java
@@ -5,29 +5,33 @@
 Create a test class to construct and call the methods of your class.
  */
 
-public class PeopleInfo {
-    public static void main(String[] args) {
-        //
-        NutritionInfo george = new NutritionInfo("George");
-	george.setName("George");
-	george.setAge(40);
-	george.setBmi(20);
-	george.setCalories(2400);
+public class PeopleInfoModded {
+  public static void main(String[] args) {
+    //
+    NutritionInfoModded george = new NutritionInfoModded("George");
+	  // george.setName("George"); /* commented out since you pass the name in as an argument in the constructor */
+	  george.setAge(40);
+	  george.setBmi((short)20); /*since shorts have no suffix (like longs, ex. 231412412414141312121L) you need to explicity cast as short*/
+	  george.setCalories(2400);
 
-	NutritionInfo carrie = new NutritionInfo("Carrie");
-	carrie.setName("Carrie");
-	carrie.setAge(23);
-	carrie.setBmi(22);
-	carrie.setCalories(2000);
+    NutritionInfoModded carrie = new NutritionInfoModded("Carrie");
+    // carrie.setName("Carrie"); /* commented out since you pass the name in as an argument in the constructor */
+    carrie.setAge(23);
+    carrie.setBmi((short)22); /*since shorts have no suffix (like longs, ex. 231412412414141312121L) you need to explicity cast as short */
+    carrie.setCalories(2000);
 
 	 // Print results
-   	 System.out.println(george.getName() + " is " + george.age + " years old ");
-	 System.out.println(george.getName() + " has a BMI of " + george.bmi);
-    	 System.out.println(george.getName() + " should consume 2400 calories a day."); 
+   /* You cannot use dot notation to access fields from other classes that are private.
+   You might want to make them public so other fields can use them, or you can call the
+   get methods you added in NutritionInfo.java to return those values */
 
-	 System.out.println(carrie.getName() + " is " + carrie.age + " years old ");
-	 System.out.println(carrie.getName() + " has a BMI of " + carrie.bmi);
-    	 System.out.println(carrie.getName() + " should consume 2000 calories a day."); 
+   	System.out.println(george.getName() + " is " + george.getAge() + " years old ");
+	  System.out.println(george.getName() + " has a BMI of " + george.getBmi());
+    System.out.println(george.getName() + " should consume 2400 calories a day.");
+
+  	System.out.println(carrie.getName() + " is " + carrie.getAge() + " years old ");
+  	System.out.println(carrie.getName() + " has a BMI of " + carrie.getBmi());
+    System.out.println(carrie.getName() + " should consume 2000 calories a day.");
 
     }
 }
