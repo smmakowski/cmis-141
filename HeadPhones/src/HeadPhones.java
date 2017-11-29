6d0312bf263b91911bb69a3a6cae14209cd882c0
smmakowski <makowski.sm@gmail.com>
2017-11-28 20:42:14 -0800

    implement getter methods

--- HeadPhones/src/HeadPhones.java
+++ HeadPhones/src/HeadPhones.java
@@ -31,32 +31,42 @@ public class HeadPhones {
  	
  	//getter methods for each field
  	public int getVolume() {
- 		
+ 		return this.volume;
  	}
  	
  	public boolean getIsPluggedIn() {
- 		
+ 		return this.pluggedIn;
  	}
  	
  	public String getManufacturer() {
- 		
+ 		return this.manufacturer;
  	}
  	
  	public Color getHeadPhoneColor() {
- 		
+ 		return this.headPhoneColor;
  	}
  	
- 	public String headphoneModel() {
- 		
+ 	public String getHeadphoneModel() {
+ 		return this.headPhoneModel;
  	}
  	
  	// setter methods for each field
- 	public void setVolume() {
+ 	/*public void setVolume(int volume) {
+ 		I have commented this, and excluded a 'setVolume()'
+ 		method because it seems like the 'changeVolume()' 
+ 		method does the same thing.
  		
- 	}
+ 		Since we were required to declare
+ 		three volume constants, and since the constructor
+ 		sets the volume to the MEDIUM constant, I assume
+ 		that those are the only options for volume and that
+ 		changeVolume.
+ 		
+ 		
+ 	} */
  	
  	public void setIsPluggedIn() {
- 		
+ 	
  	}
  	
  	public void setManufacturer() {
@@ -74,7 +84,7 @@ public class HeadPhones {
  	// toString Method
  	
  	public String toString() {
- 	
+ 		
  	}
  	
  	public void changeVolume(int volume) {
