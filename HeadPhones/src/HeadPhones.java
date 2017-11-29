b962e4a05f0e3bbd8823b75842817853b4a20a3e
smmakowski <makowski.sm@gmail.com>
2017-11-28 20:18:47 -0800

    create fields/constr/meth husks for Headphones; create instancces/loops in Test

--- HeadPhones/src/HeadPhones.java
+++ HeadPhones/src/HeadPhones.java
@@ -1,4 +1,83 @@
+/*
+	Filename: HeadPhones.java
+	Author: Stephen Makowski
+	Date: Novemeber , 2017
+	Purpose: Headphones class with fields, constructor, and methods
+*/
+
+import java.awt.Color; // import color
 
 public class HeadPhones {
-
+	// class constants
+	final int LOW = 1;
+ 	final int MEDIUM = 2;
+ 	final int HIGH = 3;
+ 	// class fields
+ 	private int volume;
+ 	private boolean pluggedIn;
+ 	private String manufacturer;
+ 	private Color headPhoneColor;
+ 	private String headPhoneModel;
+ 	
+ 	public HeadPhones() { // no args constructor
+ 		this.volume = MEDIUM; // init with efault
+ 		this.pluggedIn = false; // init with defaut
+ 		// no defaults were provided in prompt for below values
+ 		// these can be set
+ 		this.manufacturer = "";
+ 		this.headPhoneColor = null;
+ 		this.headPhoneModel = "";
+ 	}
+ 	
+ 	//getter methods for each field
+ 	public int getVolume() {
+ 		
+ 	}
+ 	
+ 	public boolean getIsPluggedIn() {
+ 		
+ 	}
+ 	
+ 	public String getManufacturer() {
+ 		
+ 	}
+ 	
+ 	public Color getHeadPhoneColor() {
+ 		
+ 	}
+ 	
+ 	public String headphoneModel() {
+ 		
+ 	}
+ 	
+ 	// setter methods for each field
+ 	public void setVolume() {
+ 		
+ 	}
+ 	
+ 	public void setIsPluggedIn() {
+ 		
+ 	}
+ 	
+ 	public void setManufacturer() {
+ 		
+ 	}
+ 	
+ 	public void setHeadPhoneColor() {
+ 		
+ 	}
+ 	
+ 	public void setHeadPhoneModel() {
+ 		
+ 	}
+ 	
+ 	// toString Method
+ 	
+ 	public String toString() {
+ 	
+ 	}
+ 	
+ 	public void changeVolume(int volume) {
+ 		
+ 	}
 }
--- HeadPhones/src/HeadPhonesTest.java
+++ HeadPhones/src/HeadPhonesTest.java
@@ -1,4 +1,19 @@
 
 public class HeadPhonesTest {
-
+	public void main(String[] args) {
+		// instantiate 3 Headphones
+		HeadPhones case1 = new HeadPhones();
+		HeadPhones case2 = new HeadPhones();
+		HeadPhones case3 = new HeadPhones();
+		// put instances in array array
+		HeadPhones[] cases = new HeadPhones[]{case1, case2, case3};
+		
+		// use setter methods to set fields to arbitrary values
+		// iterate through instance array 
+		for (int i = 0; i < cases.length; i++) {
+			System.out.println("*** CASE " + (i + 1) + " ***");// print header
+			// print return values for get/toString/changeVolume methods
+			System.out.println(""); // spacer
+		}
+	}
 }
