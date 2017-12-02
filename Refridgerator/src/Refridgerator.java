3788fabe5fec752d1621f6db182d013d62058290
smmakowski <makowski.sm@gmail.com>
2017-12-01 21:55:29 -0800

    Complete test clas

--- Refridgerator/src/Refridgerator.java
+++ Refridgerator/src/Refridgerator.java
@@ -39,35 +39,65 @@ public class Refridgerator {
 	
 	public void togglePluggedIn() {
 		if (this.isPluggedIn) {
+			System.out.println("You have unplugged the refridgerator.");
+			if (this.storage.size() > 0) {
+				System.out.println("You still have items in the refridgerator. You may want to plug it back in.");
+			}
 			this.isPluggedIn = false;
 		} else {
+			System.out.println("You have plugged the refridgerator in.");
 			this.isPluggedIn = true;
 		}
 	}
 	
 	public void setTemperature(int temperature) {
-
+		if (temperature > 0 && temperature <= 10) {
+			this.temperatureSetting = temperature;
+			System.out.println("The temperature has been set to " + this.temperatureSetting + ".");
+		} else {
+			System.out.println(temperature + " is not a valid setting. Temperature remains at " + 
+			this.temperatureSetting + ".");
+		}
 	}
-	public void checkStatus() {
+	// no argument version of setTemperature sets to recommended
+	public void setTemperature() {
 		
+		this.temperatureSetting = this.RECOMMENDED;
+	}
+	
+	public void checkStatus() {
+		String pluggedIn;
+		if(this.isPluggedIn) {
+			pluggedIn = "IS";
+		} else {
+			pluggedIn = "IS NOT";
+		}
+		System.out.print("The refridgerator " + pluggedIn + " plugged in, and the temperature is set to " + 
+		this.temperatureSetting + ".");
 	}
 	
 	public void getInfo() {
-		
+		System.out.println("This refridgerator is a " + this.model + " made by " + this.manufacturer + ".");
 	}
 	
 	public void viewContents() {
+		System.out.println("There are " + this.storage.size() + " items in the refridgerator out of " + 
+		this.capacity + " available spots.");
+		System.out.println("The items are...");
 		
+		for (int i = 0; i < this.storage.size(); i++) {
+			System.out.println((i + 1) + ": " + this.storage.get(i));
+		}
+		System.out.println("~~~ END LIST ~~~");
 	}
 	
 	public void addItem(String item) {
 		if (this.isPluggedIn) {
 			if (this.storage.size() < this.capacity) {
 				this.storage.add(item.toUpperCase());
-				System.out.println("You have added '" + item.toUpperCase() + "' to the " +
-				this.manufacturer + " " + this.model);
+				System.out.println("You have added '" + item.toUpperCase() + "' to the refridgerator");
 			} else {
-				System.out.println("The " + this.manufacturer + " " + this.model +" is full. Please use '.removeItem() to remove items.");
+				System.out.println("The refridgerator is full. Please use '.removeItem(String item) to remove items.");
 			}
 		} else {
 			System.out.println("Please plug refridgerator in before putting in items");
--- Refridgerator/src/RefridgeratorTest.java
+++ Refridgerator/src/RefridgeratorTest.java
@@ -2,8 +2,25 @@
 public class RefridgeratorTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Refridgerator fridge = new Refridgerator("General Electric", "Type R", 5);
+		
+		fridge.getInfo();
+		fridge.setTemperature(-190);
+		fridge.setTemperature(8);
+		fridge.addItem("Cheese Burger");
+		fridge.togglePluggedIn();
+		fridge.addItem("Pizza");
+		fridge.addItem("Bacon");
+		fridge.addItem("Mountain Dew");
+		fridge.addItem("Apples");
+		fridge.addItem("Lunchables");
+		fridge.addItem("Cheese Burger");
+		fridge.removeItem("MountainDew");
+		fridge.removeItem("mountain dew");
+		fridge.addItem("Cheese Burger");
+		fridge.togglePluggedIn();
+		fridge.checkStatus();
+		fridge.viewContents();
 	}
 
 }
