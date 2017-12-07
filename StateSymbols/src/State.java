366e1aaae14c1d6ebfbe9bc7c8588e2ca67cb330
smmakowski <makowski.sm@gmail.com>
2017-12-06 17:05:16 -0800

    Add fields, constructor with args, and print results

--- StateSymbols/src/State.java
+++ StateSymbols/src/State.java
@@ -1,4 +1,29 @@
 
 public class State {
-
+	private String stateName;
+	private String stateBird;
+	private String stateFlower;
+	
+	// constructor with arguments
+	public State(String name, String bird, String flower) {
+		this.stateName = autoTitleCase(name).trim(); // auto Title Case and remove trailing/leading spaces
+		this.stateBird = autoTitleCase(bird).trim();
+		this.stateFlower = autoTitleCase(flower).trim();
+	}
+	
+	public void printInfo() {
+		System.out.println(this.stateName+ ", " + this.stateBird + ", " + this.stateFlower);
+	}
+	// auto capitalizes first letter of every word
+	private static String autoTitleCase(String string) {
+		String[] chars = string.split("");
+		for (int i = 0; i < chars.length; i++) {
+			if (i == 0 || (i > 0 && chars[i - 1].equals(" "))) { // if first character or proceeding space
+				chars[i] = chars[i].toUpperCase(); // capitalize
+			} else {
+				chars[i] = chars[i].toLowerCase(); // de-capitalize
+			}
+		}
+		return String.join("", chars);
+	}
 }
--- StateSymbols/src/StateSymbolDataEntry.java
+++ StateSymbols/src/StateSymbolDataEntry.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class StateSymbolDataEntry {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// declare values for state entry
+		String name = "";
+		String bird = "";
+		String flower = "";
+		// declare array list to hold states entered
+		ArrayList<State> statesData = new ArrayList<State>();
+		State state = new State("TEST NAME", "test-flower", "tEST-Burd");
+		
+		// print results headers
+		System.out.println("**** Thank you *****");
+		System.out.println("A summary report for each State, Bird, and Flower is:");
+		state.printInfo();
+		
+		// iterate through statesData
+		for (int i = 0; i < statesData.size(); i++) {
+			// call printInfo for each state in statesData to print data
+		}
+		
+		System.out.println("Please vist our site again!");
 	}
 
 }
