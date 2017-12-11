be5f10eab763ea8a0e1e5a05cf50eb80ab9d0ab6
smmakowski <makowski.sm@gmail.com>
2017-12-11 09:57:16 -0800

    read .csv file

--- CrimeStats/src/Crime.java
+++ CrimeStats/src/Crime.java
@@ -1,4 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+
 
 public class Crime {
-
+	
+	public void readCrimeData(String fileName) throws FileNotFoundException {
+		Scanner scan = null;
+	
+		try {
+			//
+			scan = new Scanner(new File(fileName));
+			// while there are more lines to parse
+			while (scan.hasNextLine()) {
+				System.out.println(scan.nextLine());
+			}
+		} catch (FileNotFoundException e) {
+			System.err.println("Unable to find file");
+		} finally {
+			if (scan != null) {
+			scan.close();
+			}
+		}
+	}
+	
+	public void printMenu() {
+		
+	}
 }
--- CrimeStats/src/CrimeTest.java
+++ CrimeStats/src/CrimeTest.java
@@ -1,9 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
 
 public class CrimeTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-
+		Crime crimeData = new Crime();
+		try {
+			crimeData.readCrimeData("data/Crime.csv");
+		} catch (FileNotFoundException e) {
+			System.exit(1);
+		}
+		
 	}
 
 }
